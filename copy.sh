c81dac2cc78b6a8f6802d32b5594628f5584b976
ThomasLoch <thomas.loch@hotmail.com>
2021-04-15 22:05:33 -0500

    Se declara la variable de nombre de script, se inicializa y se manda a llamar la funcion run, se manda a llamar make, creacion de funcion 'is installed'.

--- copy.sh
+++ copy.sh
@@ -7,3 +7,23 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 set -e
 
 readonly SCRIPT_DIR="$(cd"$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly SCRIPT_NAME="$(basename "$0")"
+
+run
+make_backup
+
+function assert_is_installed {
+	local readonly name="$1"
+
+	if [[ ! $(command -v ${name}) ]]; then 
+		log_error "El binario '$name' se requiere pero no esta en nuestro sistema"
+	exit 1
+}
+
+function run {
+	assert_is_installed "mysql"
+	assert_is_installed "mysqldump"
+	assert_is_installed "gzip"
+
+}
+
